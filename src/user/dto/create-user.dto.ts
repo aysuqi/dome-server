6c7fd44418162394eebcca6dae64b622494d8cb2
yusunqi <ysuqi01@126.com>
2023-04-22 12:05:14 +0800

    feat: Controller 与 HTTP接口实现

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,1 +1,15 @@
-export class CreateUserDto {}
+import { ApiProperty } from '@nestjs/swagger';
+
+export class CreateUserDto {
+  @ApiProperty({ example: '18712312345' })
+  readonly phone: string;
+
+  @ApiProperty({ example: 'dome' })
+  name: string;
+
+  @ApiProperty({ example: '123456' })
+  passowrd: string;
+
+  @ApiProperty({ example: '1@1.com' })
+  email: string;
+}
--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,8 +1,22 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, HttpStatus } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  HttpStatus,
+} from '@nestjs/common';
 import { UserService } from './user.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
-import { ApiBearerAuth, ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
+import {
+  ApiBearerAuth,
+  ApiOperation,
+  ApiResponse,
+  ApiTags,
+} from '@nestjs/swagger';
 
 @Controller('users')
 @ApiTags('用户管理')

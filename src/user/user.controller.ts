dd2483b9f4f6edd61c04a298789d2663cd45c400
yusunqi <ysuqi01@126.com>
2023-04-22 11:57:17 +0800

    feat: Swagger 基础使用

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,32 +1,70 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, HttpStatus } from '@nestjs/common';
 import { UserService } from './user.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { ApiBearerAuth, ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 
-@Controller('user')
+@Controller('users')
+@ApiTags('用户管理')
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
   @Post()
+  @ApiOperation({
+    summary: '新增用户',
+  })
+  @ApiResponse({
+    status: HttpStatus.CREATED,
+    type: CreateUserDto,
+  })
+  @ApiBearerAuth()
   create(@Body() createUserDto: CreateUserDto) {
     return this.userService.create(createUserDto);
   }
 
   @Get()
+  @ApiOperation({
+    summary: '查询所有用户',
+  })
+  @ApiResponse({
+    status: HttpStatus.CREATED,
+    type: CreateUserDto,
+  })
   findAll() {
     return this.userService.findAll();
   }
 
+  @ApiOperation({
+    summary: '查询一个用户',
+  })
+  @ApiResponse({
+    status: HttpStatus.CREATED,
+    type: CreateUserDto,
+  })
   @Get(':id')
   findOne(@Param('id') id: string) {
     return this.userService.findOne(+id);
   }
 
+  @ApiOperation({
+    summary: '更新用户信息',
+  })
+  @ApiResponse({
+    status: HttpStatus.CREATED,
+    type: CreateUserDto,
+  })
   @Patch(':id')
   update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
     return this.userService.update(+id, updateUserDto);
   }
 
+  @ApiOperation({
+    summary: '删除用户',
+  })
+  @ApiResponse({
+    status: HttpStatus.CREATED,
+    type: CreateUserDto,
+  })
   @Delete(':id')
   remove(@Param('id') id: string) {
     return this.userService.remove(+id);

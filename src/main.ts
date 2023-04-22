ec990909b17b4638310c9059d8471c3a7f3e581d
yusunqi <ysuqi01@126.com>
2023-04-22 12:15:03 +0800

    feat: 添加Pipe管道及应用，实现借口校验

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { generateDocument } from './doc';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  // 添加全局管道
+  app.useGlobalPipes(new ValidationPipe());
   // 创建 swagger api 文档
   generateDocument(app);
   await app.listen(3000);
--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,15 +1,22 @@
 import { ApiProperty } from '@nestjs/swagger';
+import { IsEmail, IsNotEmpty, Length, Matches } from 'class-validator';
 
 export class CreateUserDto {
   @ApiProperty({ example: '18712312345' })
+  @IsNotEmpty()
+  @Matches(/^1\d{10}$/g, { message: '请输入手机号' })
   readonly phone: string;
 
   @ApiProperty({ example: 'dome' })
+  @IsNotEmpty()
   name: string;
 
   @ApiProperty({ example: '123456' })
+  @IsNotEmpty()
+  @Length(6, 10)
   passowrd: string;
 
   @ApiProperty({ example: '1@1.com' })
+  @IsEmail()
   email: string;
 }
